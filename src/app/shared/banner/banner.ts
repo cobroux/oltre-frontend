642d21fe34d7c28b5e8965731fb33df509ac8a96
cobroux <115625830+cobroux@users.noreply.github.com>
2026-06-24 14:37:38 +0200

    fix

--- src/app/shared/banner/banner.ts
+++ src/app/shared/banner/banner.ts
@@ -1,34 +1,25 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { UserService } from '../../core/services/user.service';
-import { UserDTO } from '../../core/models/user.dto';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-banner',
   standalone: true,
   templateUrl: './banner.html',
+    imports: [CommonModule],
   styleUrls: ['./banner.css'],
 })
 export class BannerComponent implements OnInit {
 
-  user: UserDTO | null = null;
-  loading = true;
-  error = false;
+
 
   private userService = inject(UserService);
   
-  ngOnInit(): void {
-  
-    this.userService.loadUser().subscribe({
-      next: (u) => {
-        this.user = u;
-        this.loading = false;
-        console.log("user -> " + u.username)
-      },
-      error: () => {
-        this.error = true;
-        this.loading = false;
-      }
-    });
+    user = this.userService.currentUser;
+
+    ngOnInit(): void {
+      this.userService.loadUser().subscribe();
+
   }
 }
