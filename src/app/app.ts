b06a3f0e80d81650e7f3ae96ec77e418c08560d9
cobroux <115625830+cobroux@users.noreply.github.com>
2026-06-29 11:01:34 +0200

    Amélioration

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { BannerComponent } from './shared/banner/banner';
 import { SidebarComponent } from './shared/sidebar/sidebar';
 
 @Component({
   selector: 'app-root',
-  imports: [BannerComponent, RouterOutlet, SidebarComponent],
+  imports: [BannerComponent, RouterOutlet, SidebarComponent, RouterOutlet, RouterLink, RouterLinkActive,],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
