12587c6fdfcaa01726a16a4fb739625cf5739bbb
cobroux <enzo.lvq12@gmail.com>
2026-08-14 14:14:56 +0200

    fix ci

--- src/app/features/sport/sport.spec.ts
+++ src/app/features/sport/sport.spec.ts
@@ -1,17 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { Sport } from './sport';
+import { SportComponent } from './sport';
 
 describe('Sport', () => {
-  let component: Sport;
-  let fixture: ComponentFixture<Sport>;
+  let component: SportComponent;
+  let fixture: ComponentFixture<SportComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Sport],
+      imports: [SportComponent],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(Sport);
+    fixture = TestBed.createComponent(SportComponent);
     component = fixture.componentInstance;
     await fixture.whenStable();
   });
